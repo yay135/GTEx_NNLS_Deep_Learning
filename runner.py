ec30a877e1e8c5a6308f2f582a79a25b2712ec8d
Fengyao Yan <yanfy135@gmail.com>
2023-10-24 16:40:58 -0400

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,8 +3,13 @@ import os
 
 task = os.getenv("RUN_TYPE")
 
+if len(os.listdir("/app")) == 0:
+    print("ERROR: not csv files found in the data folder.")
+    exit(1)
+
 if task is None:
     print("ERROR: no run task selected.")
+    exit(1)
 
 if task == "single_t":
     os.chdir("Deep_Learning_models/single_tissue_predict_deep/")
